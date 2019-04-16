ba4c9c16de30767151c09c3957d15ac90858f3bb
Marina Placinta <mgolovceac@gmail.com>
2019-04-15 22:36:03 -0700

    Added test cases for Go Jane

--- features/steps/product_search.py
+++ features/steps/product_search.py
@@ -2,16 +2,22 @@ from selenium.webdriver.common.by import By
 from behave import given, when, then
 from time import sleep
 
-
 SEARCH_INPUT = (By.NAME, 'q')
 SEARCH_SUBMIT = (By.NAME, 'btnK')
 RESULTS_FOUND_MESSAGE = (By.XPATH, "//div[contains(@class,'commercial-unit-desktop-top')]")
 RESULTS = (By.XPATH, "//div[@class='g']")
 
 
-@given('Open Google page')
-def open_google(context):
-    context.driver.get('https://www.google.com/')
+@given('Open Gojane page')
+def open_gojane(context):
+    context.driver.get('https://www.gojane.com/')
+    sleep(2)
+    context.driver.find_element(By.CLASS_NAME, 'fancybox-close').click()
+
+
+@given('Open Youtube page')
+def open_gojane(context):
+    context.driver.get('https://www.youtube.com/')
 
 
 @when('Input {search_word} into search field')
@@ -19,12 +25,18 @@ def input_search(context, search_word):
     search = context.driver.find_element(*SEARCH_INPUT)
     search.clear()
     search.send_keys(search_word)
-    sleep(2)
+    sleep(4)
 
 
 @when('Click on search icon')
 def click_search_icon(context):
     context.driver.find_element(*SEARCH_SUBMIT).click()
+
+
+@when('Click on {menu_item} link')
+def click_search_icon(context, menu_item):
+    sleep(2)
+    context.driver.find_element(By.LINK_TEXT, menu_item).click()
 
 
 @then('Product results for {search_word} are shown')
@@ -38,3 +50,9 @@ def verify_first_result(context, search_word):
     first_result = context.driver.find_element(*RESULTS).text
     print('\n{}'.format(first_result))
     assert search_word in first_result, "Expected word '{}' in message, but got '{}'".format(search_word, first_result)
+
+
+@then('Top navigation is {all_shoes}')
+def verify_found_results_text(context, all_shoes):
+    result_message = context.driver.find_element(By.CLASS_NAME, 'current').text
+    assert all_shoes in result_message, "Expected word '{}' in message, but got '{}'".format(all_shoes, result_message)
--- features/tests/product_search.feature
+++ features/tests/product_search.feature
@@ -1,9 +1,17 @@
 # Created by Svetlana at 4/4/19
 Feature: Test Scenarios for Search functionality
 
-  Scenario: User can search for dresses
-    Given Open Google page
-    When Input dress into search field
-    And Click on search icon
-    Then Product results for dress are shown
-    And First result contains dress
+  Scenario: User can open shoes page
+    Given Open Gojane page
+    When Click on SHOES link
+    Then Top navigation is All Shoes
+
+  Scenario: User can open dresses page
+    Given Open Gojane page
+    When Click on DRESSES link
+    Then Top navigation is All Dresses
+
+  Scenario: User can open clothing page
+    Given Open Gojane page
+    When Click on CLOTHING link
+    Then Top navigation is All Clothes
